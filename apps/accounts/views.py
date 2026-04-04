9a02c58e4f4e1e8cd9a998c770befe8e0998def3
randrade <rubenandraderuben@gmail.com>
2026-04-04 15:31:38 -0500

    fix 041,fix 040

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -358,6 +358,13 @@ class EditClientProfileView(LoginRequiredMixin, UserPassesTestMixin, FormView):
         kwargs['user'] = self.request.user
         return kwargs
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        from apps.accounts.models import ClientProfile
+        profile, _ = ClientProfile.objects.get_or_create_for_user(self.request.user)
+        context['profile'] = profile
+        return context
+
     def form_valid(self, form):
         """Use services to update client profile"""
         success, _, error = services.update_client_profile(
@@ -395,6 +402,12 @@ class EditTutorProfileView(LoginRequiredMixin, UserPassesTestMixin, FormView):
         kwargs['user'] = self.request.user
         return kwargs
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        profile, _ = TutorProfile.objects.get_or_create_for_user(self.request.user)
+        context['profile'] = profile
+        return context
+
     def form_valid(self, form):
         """Use services to update tutor profile"""
         success, _, error = services.update_tutor_profile(
