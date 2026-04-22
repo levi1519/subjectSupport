7498e7cbad5c1e09989be5d23b5a33ff6abb33d6
randrade <rubenandraderuben@gmail.com>
2026-04-21 20:26:47 -0500

    fix: add migrations to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+echo "Running database migrations..."
+DEBUG=True python manage.py migrate
 echo "Running collectstatic..."
 DEBUG=True python manage.py collectstatic --noinput
 echo "Build complete."
