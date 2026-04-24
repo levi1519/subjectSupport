f39ad6dda6e83b9f89fb2ede15e0bc341a2762df
randrade <rubenandraderuben@gmail.com>
2026-04-23 20:09:04 -0500

    feat: flujo aprobacion tutor, PlatformConfig, document_file, university, notif expiry

---
+++ check_db.py
@@ -0,0 +1,17 @@
+﻿import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'subjectSupport.settings')
+
+import django
+django.setup()
+
+from apps.accounts.models import TutorProfile, KnowledgeArea
+
+print('=== CIUDADES ===')
+for c in TutorProfile.objects.exclude(city='').exclude(city=None).values_list('city', 'country').distinct():
+    print(c)
+
+print('=== AREAS ===')
+for area in KnowledgeArea.objects.prefetch_related('subjects').all():
+    print(area.name)
+    for s in area.subjects.all():
+        print('  ' + s.name)
